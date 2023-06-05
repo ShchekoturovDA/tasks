41828f8a1ad81997f2f8355452112c1beb726d03
ShchekoturovDA <shchekoturovdmitr@mail.ru>
2023-06-05 22:55:52 +0300

    l15

--- lectures/l15/src/main/java/org/tasks/Main.java
+++ lectures/l15/src/main/java/org/tasks/Main.java
@@ -8,12 +8,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String inpcom = scan.nextLine();
-        List<String> inplist = List.of(inpcom.split(" "));
-        Map<String, String> inpmap = new HashMap<String, String>();
-        Set <String> inpset = new HashSet<String>();
-        inplist.stream()
-                .filter(x -> x.charAt(0) == '-')
-                .collect(Collectors.toSet());
-         = inplist;
+        //List<String> inplist = List.of(inpcom.split(" "));
+        Map<String, String> inpmap
+                = Arrays.stream(inpcom.split("-"))
+                .filter(x -> x != "")
+                .collect(Collectors.toMap(
+                        value
+                                -> '-' + value.split(" ")[0],
+                        value ->  value.split(" ", 2)[1])
+                );
+        System.out.println(Arrays.toString(inpcom.split("-")));
+        inpcom =
+                inpmap.entrySet().stream()
+                .map(key -> key.getKey() + " " + inpmap.get(key.getKey()) + " ")
+                        .collect(Collectors.joining(""));
+        System.out.println(inpmap);
+        System.out.println(inpcom);
     }
 }
